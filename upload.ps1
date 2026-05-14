3a4ac08bd3070b24e5541a1aed90b690c0ac1bc7
Troy Miller <troyjm28@g.eips.ca>
2026-05-14 11:37:32 -0600

    Initial commit

--- upload.ps1
+++ upload.ps1
@@ -3,6 +3,6 @@ git init
 git add .
 git commit -m "Initial commit"
 
-git remote add origin https://github.com/bazinga637/TCode/tcode
+#git remote add origin https://github.com/bazinga637/TCode
 git branch -M main
 git push -u origin main
