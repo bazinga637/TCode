ecb912515bc09afa1f1b48901e5aac283ae28d94
Troy Miller <troyjm28@g.eips.ca>
2026-05-15 10:19:00 -0600

    test commit

---
+++ pull.ps1
@@ -0,0 +1,6 @@
+
+# navigate to folder to clone to and run git clone, any further updates will use git pull
+
+git clone https://github.com/bazinga637/TCode.git
+
+git pull origin main
--- upload.ps1
+++ upload.ps1
@@ -1,8 +1,16 @@
+git add .
+git commit -m "test commit"
+git push
+
+#####################################################
+<#
 
 git init
 git add .
 git commit -m "Initial commit"
 
-#git remote add origin https://github.com/bazinga637/TCode
+git remote add origin https://github.com/bazinga637/TCode.git
 git branch -M main
 git push -u origin main
+
+#>
