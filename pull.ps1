431fcb6e00fda372e1055d98e5d8d0e8f7fc95e4
bazinga637 <troyjkmiller@gmail.com>
2026-05-22 21:26:52 -0600

    test commit

--- pull.ps1
+++ pull.ps1
@@ -1,8 +1,8 @@
 # THIS SCRIPT WILL DELETE EVERYTHING AFTER CLONING
 # navigate to folder to clone to and run git clone, any further updates will use git pull
 
-git clone https://github.com/bazinga637/TCode.git ../temp/TCode
+git clone https://github.com/bazinga637/TCode.git ../temp/TCode # clone to temp folder
 
 git pull origin main
 
-Move-Item -Path "\home\mjkstra\Documents\temp\TCode" -Destination "\home\mjkstra\Documents" -Force
+Move-Item -Path "\home\mjkstra\Documents\temp\TCode" -Destination "\home\mjkstra\Documents" -Force # replace from temp folder to actual folder
