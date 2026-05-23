527592a9d37006fbbb5944ee178366b50a66dfcd
bazinga637 <troyjkmiller@gmail.com>
2026-05-22 22:26:41 -0600

    update

--- pull.ps1
+++ pull.ps1
@@ -1,8 +1,8 @@
 # THIS SCRIPT WILL DELETE EVERYTHING AFTER CLONING
 # navigate to folder to clone to and run git clone, any further updates will use git pull
 
-git clone https://github.com/bazinga637/TCode.git ../temp/TCode # clone to temp folder
+git clone https://github.com/bazinga637/TCode.git ../TCodetemp/TCode # clone to temp folder
 
 git pull origin main
 
-Move-Item -Path "\home\mjkstra\Documents\temp\TCode" -Destination "\home\mjkstra\Documents" -Force # replace from temp folder to actual folder
+Move-Item -Path "\home\mjkstra\Documents\TCodetemp\TCode" -Destination "\home\mjkstra\Documents" -Force # replace from temp folder to actual folder
