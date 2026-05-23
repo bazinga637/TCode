bcbb36cfd602f64967614e4bd5bd11e4eb263b52
bazinga637 <troyjkmiller@gmail.com>
2026-05-22 20:55:35 -0600

    test commit

--- pull.ps1
+++ pull.ps1
@@ -1,5 +1,6 @@
-
+# THIS SCRIPT WILL DELETE EVERYTHING BEFORE CLONING
 # navigate to folder to clone to and run git clone, any further updates will use git pull
+rm -rf *
 
 git clone https://github.com/bazinga637/TCode.git .
 
